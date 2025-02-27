e3976aadc41f9eb4fba9a9794ca2dc0e03b6df33
Shreyas Dankhade <85983813+ShreyasDankhade@users.noreply.github.com>
2025-02-27 21:19:01 +0530

    Update main.tf
    s

--- main.tf
+++ main.tf
@@ -34,9 +34,9 @@ resource "aws_iam_role" "lambda_role" {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Action": "sts:AssumeRole",
-      "Principal": { "Service": "lambda.amazonaws.com" },
-      "Effect": "Allow"
+      "Effect": "Allow",
+      "Action": "iam:PassRole",
+      "Resource": "${aws_iam_role.lambda_role.arn}"
     }
   ]
 }
