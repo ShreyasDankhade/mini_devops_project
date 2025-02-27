6684095f6e7690badaa935c5797a26c029ad6c67
Shreyas Dankhade <85983813+ShreyasDankhade@users.noreply.github.com>
2025-02-27 13:50:12 +0530

    Update main.py
    New one

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-
 def read_data_from_s3(bucket_name, object_key):
     s3 = boto3.client('s3')
     response = s3.get_object(Bucket=bucket_name, Key=object_key)
@@ -49,7 +48,6 @@ def push_data_to_rds(data):
     finally:
         connection.close()
 
-
 def handler(event, context):
     """
     AWS Lambda handler function.
@@ -62,12 +60,10 @@ def handler(event, context):
     s3_object_key = os.environ.get('S3_OBJECT_KEY')
 
     if not s3_bucket or not s3_object_key:
-        raise Exception(
-            "S3_BUCKET and S3_OBJECT_KEY environment variables must be set.")
+        raise Exception("S3_BUCKET and S3_OBJECT_KEY environment variables must be set.")
 
     try:
-        logger.info("Reading data from S3 bucket '%s', key '%s'.",
-                    s3_bucket, s3_object_key)
+        logger.info("Reading data from S3 bucket '%s', key '%s'.", s3_bucket, s3_object_key)
         data = read_data_from_s3(s3_bucket, s3_object_key)
     except Exception as e:
         logger.error("Failed to read data from S3: %s", e)
@@ -86,12 +82,11 @@ def handler(event, context):
         "body": "Data successfully transferred from S3 to RDS"
     }
 
-
 # For local testing
 if __name__ == '__main__':
     # Set these values to test locally; in Lambda, use environment variables.
-    os.environ['S3_BUCKET'] = 'go-digital-devops'
-    os.environ['S3_OBJECT_KEY'] = 'sample-data.txt'
+    os.environ['S3_BUCKET'] = 'go-digital-devops'  # Ensure this bucket exists
+    os.environ['S3_OBJECT_KEY'] = 'sample-data.txt'  # Ensure this object exists
     os.environ['RDS_HOST'] = 'go-digital-db.c5oicwa8wscl.ap-south-1.rds.amazonaws.com'
     os.environ['RDS_USER'] = 'admin'
     os.environ['RDS_PASSWORD'] = 'go-digital-db'
